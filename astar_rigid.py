5b4a919f101e4f811bb610b9b809c6aec64d74c5
SmritiUMD <sgupta23@terpmail.umd.edu>
2020-03-17 20:04:47 -0400

    Adding the code for a star including a goal_check function to check the goal, storing the nodes information, parent information and printing them all

--- astar_rigid.py
+++ astar_rigid.py
@@ -11,25 +11,28 @@ goal= 10,10
 r = input("enter value of radius") #taking input for radius
 
 c = input("enter value of clearance") #taking input for clearance
-R = input("enter value of resolution") #taking input for resolution
+R = int(input("enter value of resolution")) #taking input for resolution
 
 
 d = int(r+c)
+solvable=True
 
 #start_time = time.time() #taking the run time start
-def checkr(r):
+def checkr(R):
     if (250%R)!=0  or(150%R)!=0:
         print("Please enter an achivable resolution")
         exit()
     else:
         pass
+
+
 def obstacle(x,y):
     flag=True
     if (y>=(3/5)*x+(25-d)/R) and (y>=(-3/5)*x+(295-d)/R):
         flag=False
-    elif (y>=(3/5)*x+55+d) or (y>=(-3/5)*x+325+d):
+    elif (y>=(3/5)*x+(55+d)/R) or (y>=(-3/5)*x+(325+d)/R):
         flag=True
-    elif (x-math.ceil(225/R))**2+(y-math.ceil(50/r))**2<=(math.ceil((25+d)/R))**2:
+    elif (x-math.ceil(225/R))**2+(y-math.ceil(50/R))**2<=(math.ceil((25+d)/R))**2:
         flag=False
     elif (x-(math.ceil(150/R)))**2/(math.ceil((40+d)/R))**2+(y-(math.ceil(100/R)))**2/(math.ceil((20+d)/R))**2<=1:
         flag=False
@@ -56,7 +59,7 @@ def obstacle(x,y):
  #writing the condition for the case when start or goal node are defined in an obstacle
 if (obstacle(initial[0],initial[1])==False or (obstacle(goal[0],goal[1])==False)):
         print("position not allowed")
-        flag=False
+        solvable=False
 else:
     pass
 
@@ -65,6 +68,16 @@ def heuristics(current, goal): #defining heuristic function as euclidian distanc
     h=math.sqrt((current[0]-goal[0])**2 + (current[1]-goal[1])**2)
     
     return h
+
+
+def goal_check(i,j):  # function to check if the explored point is inside threshold area around the goal or not
+    if (i-goal[0])**2+(j-goal[1])**2<=(1.5)**2:
+        k=2
+    else:
+        k=0
+    return k
+
+
 cost_list=[]
 costh_list=[]
 cost=float('Inf')
@@ -76,8 +89,8 @@ theta_s= 0 #(initial theta =0)
 r=1 # step size(can be modified)
 
 plot=np.zeros((600,300), np.uint8)  # creating a matrix to append information of visited nodes
-visited = set([]) #
-visited[initial[0],initial[0]]
+visited = set([]) #\
+
 class Node:
     def __init__(self, pos, cost, parent): #creating objects for position, cost and parent information
         self.pos = pos
@@ -91,8 +104,8 @@ def explore(node): #defining the function for exploring using a star
     j = node.y
     valid_paths=[]
     for m in range(1,7):
-        i=initial[0]+r*(math.cos(math.radians(m)))
-        j= initial[1]+r*(math.sin(math.radians(m)))
+        i=round(initial[0]+r*(math.cos(math.radians(m))))
+        j= round(initial[1]+r*(math.sin(math.radians(m))))
         k=theta_s+m*30
         if obstacle(i,j)==True:  #checking for the obstacle space
             # print ('in 2nd if exp')
@@ -102,14 +115,63 @@ def explore(node): #defining the function for exploring using a star
             valid_paths.append([(i,j), cost,cost_h,cost_go])
         return valid_paths #returning all the valid paths that pass the conditions
 
-
-q = PriorityQueue() #defining a priority queue
-visited = set([]) #creating visited nodes
-node_objects = {}
-
-####
 distance = {}
-for i in range(0, 600):
-    for j in range(0, 300):
+for i in range(0, math.ceil(600/R)):
+    for j in range(0, math.ceil(300/R)):
         distance[str([i, j])] = 99999999 #making the value of all the unvisited nodes as infinity
 #####
+
+q = PriorityQueue() #defining a priority queue
+node_objects = {}
+distance[str(initial)] = 0 
+visited.add(str(initial))
+node = Node(initial, 0, None)
+node_objects[str(node.pos)] = node
+q.put([node.cost, node.pos])  # adding the cost values in a priority queue
+reached = False
+
+
+####
+
+if solvable:  #logic for astar to check if the nodes traversed is a goal else continue
+    while not q.empty():
+        node_temp = q.get()
+        node = node_objects[str(node_temp[1])]
+        if  goal_check(node_temp[1][0], node_temp[1][1] )==2:
+            print("Reached")
+            node_objects[str(goal)] = Node(goal, node_temp[0], node)
+            reached = True
+            break
+
+        for next_node, cost, cost_h, cost_go in explore(node):
+
+            if str(next_node) in visited: #defining all the visited nodes and adding the cost values 
+                cost_temp = cost + distance[str(node.pos)]
+                if cost_temp < distance[str(next_node)]:
+                    distance[str(next_node)] = cost_temp
+                    node_objects[str(next_node)].parent = node
+            else:
+                visited.add(str(next_node)) #adding the next node value to the visited node
+                #img_show[next_node[1], next_node[0], :] = np.array([0,0,255])
+                absolute_cost = cost + distance[str(node.pos)]
+                distance[str(next_node)] = absolute_cost
+                new_node = Node(next_node, absolute_cost, node_objects[str(node.pos)])
+                node_objects[str(next_node)] = new_node
+                q.put([absolute_cost, new_node.pos])  #using the queue to add get the least cost 
+                print(visited)
+
+    #print("--- %s seconds ---" % (time.time() - start_time)) #printing the total time taken to run the logic
+              
+    #cv2.imshow('img', img_show)
+    #cv2.waitKey(10)
+    goal_node = node_objects[str(goal)]
+    parent_node = goal_node.parent  #adding the previous node traveled from the goal using backtracking to the parent node
+    while parent_node:
+        print(parent_node.pos, parent_node.cost)
+        #img_show[parent_node.pos[1], parent_node.pos[0],:] = np.array([255,0,0]) #using cv2.imshow to print the final path using backtracking
+        parent_node = parent_node.parent
+    #cv2.imshow('img', img_show)
+    #cv2.waitKey(0)
+
+
+
